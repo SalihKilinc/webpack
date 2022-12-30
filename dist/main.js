68b3b3884a0b3cb4d834afdb986152ac553eccd0
salih <salihkilinc80@gmail.com>
2022-12-30 20:45:00 +0100

    v1

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(() => { "use strict"; alert("hello webpack"), localStorage.setItem("token", "doichjodjcadspkcèasdifg'0vtjàpbjsidfkjrvoahé") })();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { setItem } from "./utils/storage";
+
+alert("hello webpack");
+
+
+setItem("token" , "doichjodjcadspkcèasdifg'0vtjàpbjsidfkjrvoahé")
